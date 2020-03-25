6ff907f8f8f0c0fc1195d681f3186507e30a7cef
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-25 15:38:58 -0700

    container component created

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import NavBar from './components/NavBar'
+import Container from './components/Container'
 
 function App() {
   return (
+    <div>
     <NavBar />
+    <Container />
+    </div>
   );
 }
 
---
+++ client/src/components/Container/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css'
+
+function Container() {
+
+    return (
+        <div className="uk-container">
+            Container
+        </div>
+    )
+}
+
+export default Container;
