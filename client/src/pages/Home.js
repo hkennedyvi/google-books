f1ab37dbb413746fee757b8b603de028946ac36a
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-26 16:49:02 -0700

    components added to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Banner from '../components/Banner';
-import NavBar from '../components/NavBar';
 import Search from '../components/Search';
-import ResultsCard from '../components/ResultsCard';
 import ResultsSection from '../components/ResultsSection';
 
 function Home() {
     return (
-        <h1>HOMEPAGE YO</h1>
+        <div>
+            <Banner />
+            <Search />
+            <ResultsSection />
+        </div>
     )
 }
 
