8a64b334fd5ac2735b18c05f836a5d4b5a8b9c53
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-25 17:27:49 -0700

    results card created

--- client/src/components/ResultsSection/index.jsx
+++ client/src/components/ResultsSection/index.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
-import './ResultsSection.css'
+import './ResultsSection.css';
+import ResultsCard from '../ResultsCard';
 
 function ResultsSection() {
 
     return (
         <div id="results-main" className="uk-section">
-            <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2 uk-margin" uk-grid>
-                <div class="uk-card-media-left uk-cover-container">
-                    {/* Image needs fixing, floats above body content */}
-                    {/* <img src="https://picsum.photos/200" alt="" uk-cover /> */}
-                </div>
-                <div>
-                    <div class="uk-card-body">
-                        <h3 class="uk-card-title">TITLE:</h3>
-                        <p>AUTHOR:</p>
-                        <p>DESCRIPTION: This book is crazy a lot of stuff happens and people say woaaahhhh at the end.</p>
-                    </div>
-                </div>
-            </div>
-
-
-            <div class="uk-card uk-card-default uk-grid-collapse uk-child-width-1-2 uk-margin" uk-grid>
-                <div class="uk-card-media-left uk-cover-container">
-                    {/* <img src="https://picsum.photos/200" alt="" uk-cover /> */}
-                </div>
-                <div>
-                    <div class="uk-card-body">
-                        <h3 class="uk-card-title">TITLE:</h3>
-                        <p>AUTHOR:</p>
-                        <p>DESCRIPTION: This book is crazy a lot of stuff happens and people say woaaahhhh at the end.</p>
-                    </div>
-                </div>
-            </div>
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
+            <ResultsCard />
         </div>
-
-
     )
 }
 
