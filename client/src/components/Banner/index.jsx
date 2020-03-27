98e730883f58505c8b4d612b2b45097284714e06
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-26 18:27:19 -0700

    banner style edit

--- client/src/components/Banner/index.jsx
+++ client/src/components/Banner/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Banner.css'
 
 function Banner() {
 
@@ -6,14 +7,23 @@ function Banner() {
         <div class="uk-section uk-section-small uk-section-muted">
             {/* <div class="uk-container"> */}
 
-                <h1 class="uk-text-center">(REACT) GOOGLE BOOK FINDER</h1>
+            <h1 class="uk-text-center">(REACT) GOOGLE BOOK FINDER</h1>
 
-                <div class="uk-flex uk-flex-center">
-                    <div class="uk-width-1-4 uk-card uk-card-default uk-card-body">FIND.</div>
-                    <div class="uk-width-1-4 uk-card uk-card-default uk-card-body uk-margin-left">SAVE.</div>
-                    <div class="uk-width-1-4 uk-card uk-card-default uk-card-body uk-margin-left">READ.</div>
+            <div class="uk-flex uk-flex-center">
+                <div class="uk-width-1-4 uk-card uk-card-default uk-card-body uk-text-center">
+                    <i class="banner-icon fas fa-eye"></i>
+                    <p>FIND.</p>
+                </div>
+                <div class="uk-width-1-4 uk-card uk-card-default uk-card-body uk-margin-left uk-text-center">
+                    <i class="banner-icon fas fa-save"></i>
+                    <p>SAVE.</p>
+                </div>
+                <div class="uk-width-1-4 uk-card uk-card-default uk-card-body uk-margin-left uk-text-center">
+                    <i class="banner-icon fas fa-book-reader"></i>
+                    <p>READ.</p>
                 </div>
             </div>
+        </div>
         // </div>
     )
 }
