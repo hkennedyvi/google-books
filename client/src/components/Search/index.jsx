bdd14f741d0920f9da8cb1b9986d337e0de1b27f
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-26 22:28:07 -0700

    search and input change handlers created

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Search.css';
 
-function Search() {
+function Search(props) {
 
     return (
         <div class="uk-section uk-section-small uk-section-muted search">
         <form action="">
-            <input className="uk-input uk-form-width-large" type="text" placeholder="Find a book..." />
-            <button className="uk-button uk-button-default"><i className="fas fa-search"></i></button>
+            <input 
+            className="uk-input uk-form-width-large" type="text" placeholder="Find a book..." 
+            onChange={(event) => {props.handleInputChange(event) }} />
+            <button 
+            className="uk-button uk-button-default" 
+            value=""
+            onClick={(event) => { props.handleSearch(event) }}>
+                <i className="fas fa-search"></i>
+                </button>
         </form>
         </div>
     )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,29 +6,42 @@ import ResultsSection from '../components/ResultsSection';
 
 function Home() {
 
-    const [books, setBooks] = useState([])
+    const [books, setBooks] = useState([]);
+    const [searchTerm, setSearchTerm] = useState();
 
-    useEffect(() => {
-        loadBooks()
-    }, [])
+    // useEffect(() => {
+    //     loadBooks()
+    // }, [])
 
-    // Loads all books and sets them to books
-    function loadBooks() {
-        API.populateLibrary()
-            .then(res => {
-                // console.log(res.data.items);
-                setBooks(res.data.items);
-                // console.log(books);
-            }
-            )
-            .catch(err => console.log(err));
-    };
+    // // Loads all books and sets them to books
+    // function loadBooks() {
+    //     API.populateLibrary()
+    //         .then(res => {
+    //             // console.log(res.data.items);
+    //             setBooks(res.data.items);
+    //             // console.log(books);
+    //         }
+    //         )
+    //         .catch(err => console.log(err));
+    // };
+
+    function handleInputChange(event) {
+        let userSearch = event.target.value;
+        setSearchTerm(userSearch);
+        console.log("current search", searchTerm);
+    }
+
+    function handleSearch(event) {
+        event.preventDefault();
+        console.log("YEELLLLOOOO");
+        console.log(event.target.value);
+    }
 
     return (
         <div>
             <Banner />
-            <Search />
-            <ResultsSection books={books}/>
+            <Search handleSearch={handleSearch} handleInputChange={handleInputChange} />
+            <ResultsSection books={books} />
         </div>
     )
 }
