9dc50d7a3cc186cd1e3515025ac576826d075f34
Howard Kennedy <hkennedyvi@gmail.com>
2020-03-30 14:53:43 -0700

    mLab config

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user1:password1@ds017193.mlab.com:17193/heroku_phft3tb1", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
